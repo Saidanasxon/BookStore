aad061d080fa5814d1bbecaf2c76a6e5ac214d56
Saidanasxon <saydullayevsaidanasxon@gmail.com>
2024-06-19 17:48:33 +0500

    'second_commit'

--- clients/models.py
+++ clients/models.py
@@ -9,6 +9,10 @@ class Cart(models.Model):
     def __str__(self):
         return self.book.title
     
+    @property
+    def total_price(self):
+        return self.book.price * self.quantity
+    
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
